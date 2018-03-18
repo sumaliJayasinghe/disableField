a7a663db79fea9f21408549585fa86ec8228a90d
sumaliJayasinghe <sukijayasinghe@gmail.com>
2018-03-18 15:33:15 +0800

    Create CustomDirectiveController.js

---
+++ CustomDirectiveController.js
@@ -0,0 +1,11 @@
+app.controller('CustomDirectiveController', function() {
+
+    $scope.checkEditable = function() {
+    
+        // your condinal check
+        //return true or false
+        return false;
+        
+    }
+    
+});
